91fc2f7c3f8e7fd62de1886086d46f1755041411
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2021-12-30 22:24:45 +1100

    Added iteration checkbox to welcome screen

--- Data Viewer/scripts/app.js
+++ Data Viewer/scripts/app.js
@@ -5,6 +5,7 @@ formUpload.addEventListener("change", function(event){
     var townDataList;
     var foundTownData = false;
     rawFiles = formUpload.files
+    var found = false;
     
     for(var i=0; i<rawFiles.length; i++){
         var reader = new FileReader();
@@ -18,25 +19,54 @@ formUpload.addEventListener("change", function(event){
             }
             if(iterationList.length == rawFiles.length - 1 && foundTownData){
                 showStats(iterationList, townData)
+                found = true
             }
         }
         reader.readAsText(rawFiles[i])
     }
+
 });
 
 
 function showStats(iterationList, townData){
-    console.log(townData)
+    console.log("YES")
     var statusContainer = document.getElementById("statusContainer");
     statusContainer.classList.remove("hidden");
-
+    
     var statusHTML = ""
     statusHTML += `
-        <h2>Town stats </h2>
-        <h3>Town Name: ` + iterationList[0]["townName"] + `</h3>
-        <h3>Population: ` + townData["general"][0]["population"] + `</h3>
+    <div class="statsContainer">
+        <div class="townStats statContainer">
+            <h2>Town stats </h2>
+            <h3>Town Name: ` + iterationList[0]["townName"] + `</h3>
+            <h3>Population: ` + townData["general"][0]["population"] + `</h3>
+        </div>
+        <div class="iterationStats statContainer">
         <h2>Iterations</h2>
+        <ul class="iterationList">
+    `
+    console.log("HI")
+    for(var i=0; i<iterationList.length; i++){
+        console.log("REE")
+        statusHTML += `
+        <li class="iterationListItem">
+            <div class="iterationItemContainer">
+                <h3> ` + iterationList[i].iterationName + `</h3>
+                <input type="checkbox" class="iterationCheckbox", id="iteration" + ` + i + ` checked>
+            </div>
+        </li>
+        `
+    }
+    
+    console.log("HI")
+    statusHTML += `
+            </ul>
+        </div>
+    </div>
+
+    <button class="submitBtn">Submit</button>
     `
 
+    console.log(statusHTML)
     statusContainer.innerHTML = statusHTML
 }
